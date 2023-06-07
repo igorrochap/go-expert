d043a1ea1d0dc7b8b5f3a403d8e3a5220a5f400c
igorrochap <igor.systemof@gmail.com>
2023-06-06 21:45:46 -0300

    loopings

---
+++ modulo-1/21/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	// for i := 0; i < 10; i++ {
+	// 	println(i)
+	// }
+
+	// numbers := []string{"hello", "world", "go"}
+	// for key, value := range numbers {
+	// 	println(key, value)
+	// }
+
+	// i := 0
+	// for i < 10 {
+	// 	println(i)
+	// 	i++
+	// }
+
+	for {
+		println("Hello World! Go infinite loop")
+	}
+}
