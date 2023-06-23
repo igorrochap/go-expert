3f062e211b973d770c23184e76689508154509bb
igorrochap <igor.systemof@gmail.com>
2023-06-23 20:37:31 -0300

    manipulating headers

--- modulo-2/starting_with_http/main.go
+++ modulo-2/starting_with_http/main.go
@@ -8,5 +8,18 @@ func main() {
 }
 
 func searchByCEP(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cep := request.URL.Query().Get("cep")
+	if cep == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
 	writer.Write([]byte("Hello, World!"))
 }
