152752779d371d62c0b6868ec67717dd403c8b4d
igorrochap <igor.systemof@gmail.com>
2024-04-12 18:11:22 -0300

    update and select row

--- 4 - database/1/main.go
+++ 4 - database/1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
@@ -28,13 +29,23 @@ func main() {
 	}
 	defer db.Close()
 	product := newProduct("Notebook", 4699.99)
-	err = insertProduct(db, *product)
+	err = insertProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
+	product.Price = 4000.99
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+	result, err := findByID(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Product %v has the price of %.2f\n", result.Name, result.Price)
 }
 
-func insertProduct(db *sql.DB, product Product) error {
+func insertProduct(db *sql.DB, product *Product) error {
 	stmt, err := db.Prepare("insert into products(id, name, price) values (?, ?, ?)")
 	if err != nil {
 		return err
@@ -45,4 +56,31 @@ func insertProduct(db *sql.DB, product Product) error {
 		return err
 	}
 	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func findByID(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select * from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
 }
